15f7a8233b794fefbb5feade6532460542493760
samyang <slyangsam@outlook.com>
2025-03-17 19:23:22 -0400

    add app stock

--- sycee/sycee/urls.py
+++ sycee/sycee/urls.py
@@ -16,7 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('stock/', include('stock.urls')),
+]
+
+# stock is the default application for now
+urlpatterns += [
+    path('', RedirectView.as_view(url='/stock/', permanent=True)),
+]
+
+# Temp static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
