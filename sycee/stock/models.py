89d0a63ab3c303225ff45d6acf99ab2ebbaa2406
samyang <slyangsam@outlook.com>
2025-03-17 20:05:55 -0400

    add daily stock model and blank homepage

--- sycee/stock/models.py
+++ sycee/stock/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class DailyStock(models.Model):
+    id = models.CharField(primary_key=True, max_length=21, editable=False)
+    symbol = models.CharField(max_length=10)
+    date = models.DateField()
+    open = models.CharField(max_length=18)
+    high = models.CharField(max_length=18)
+    low = models.CharField(max_length=18)
+    close = models.CharField(max_length=18)
+    volume = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['date']
+
+    def save(self, *args, **kwargs):
+        # Generate ID for new records
+        if not self.id:
+            self.id = f"{self.symbol}-{self.date.strftime('%Y-%m-%d')}"
+        # Prevent changing symbol/date for existing records
+        else:
+            existing = StockRecord.objects.get(pk=self.id)
+            if existing.symbol != self.symbol or existing.date != self.date:
+                raise ValidationError("Cannot modify symbol or date for existing records")
+                
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.id} - Open: {self.open}, Close: {self.close}"
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of the model."""
+        return reverse('stock-day-price', args=[str(self.id)])
--- sycee/stock/views.py
+++ sycee/stock/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import DailyStock
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    # num_records = DailyStock.objects.all().count()
+    num_records = 0
+
+    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+
+    context = {
+        'num_records': num_records,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
